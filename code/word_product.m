fb44a8c6129e9da48c172a250efc5a03b28fe84e
teja-91 <raviteja.chatta@gmail.com>
2015-07-19 12:52:22 +0530

    word_product

---
+++ code/word_product.m
@@ -0,0 +1,12 @@
+% If the input string s is a word like 'hello', then the output word product p is a number based on the correspondence a=1, b=2, ... z=26. Assume the input will be a single word, although it may mixed case. Note that A=a=1 and B=b=2.
+% 
+% So
+% 
+%  s = 'hello'
+% means
+% 
+%  p = 8 * 5 * 12 * 12 * 15 = 86400
+
+function p = word_product(s)
+  p = prod((double(lower(s))-96));
+end
