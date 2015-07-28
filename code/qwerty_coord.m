366c3db12dda87c7a22737f70b084b9fe85c05c9
teja-91 <raviteja.chatta@gmail.com>
2015-07-28 11:09:18 +0530

    directly used the qwerty character array

---
+++ code/qwerty_coord.m
@@ -0,0 +1,9 @@
+
+
+function [r,c] = qwerty_coord(key)
+ k=['1':'9','0';
+    'q','w','e','r','t','y','u','i','o','p';
+    'a','s','d','f','g','h','j','k','l',';';
+    'z','x','c','v','b','n','m',',','.','/'];
+[r,c]=ind2sub(size(k),find(k==key))
+end
