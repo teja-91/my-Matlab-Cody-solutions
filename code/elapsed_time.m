6cfb93877e7df2a2597dbec9f9e0b667933537b5
teja-91 <raviteja.chatta@gmail.com>
2015-07-28 12:15:40 +0530

    minor changes

--- code/elapsed_time.m
+++ code/elapsed_time.m
@@ -9,13 +9,6 @@
 %  Output elapsed is 1.11
 
 function elapsed = elapsed_time(d1,d2)
-% d1(regexp(d1,'[:/]'))=' ';
-% d2(regexp(d2,'[:/]'))=' ';
-% d1=str2num(d1)
-% d2=str2num(d2)
-% p=d2-d1
-% numdays=(p(1)*(365.2422))+(p(2)*(365.2422/12))+(p(3))
-% elapsed=vpa((numdays*24)+p(4)+(p(5)/60)+(p(6)/3600))
 elapsed=(etime(datevec(d2),datevec(d1)))/3600;
   
 end
--- code/qwerty_coord.m
+++ code/qwerty_coord.m
@@ -5,5 +5,5 @@ function [r,c] = qwerty_coord(key)
     'q','w','e','r','t','y','u','i','o','p';
     'a','s','d','f','g','h','j','k','l',';';
     'z','x','c','v','b','n','m',',','.','/'];
-[r,c]=ind2sub(size(k),find(k==key))
-end
+[r,c]=ind2sub(size(k),find(k==key));
+ends
