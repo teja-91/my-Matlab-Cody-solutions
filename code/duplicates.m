57d079b9a6a3b1958028418d3dee585b899b9c3f
teja-91 <raviteja.chatta@gmail.com>
2015-08-14 13:43:18 +0530

    at first id did not understand how to solve this problem
    then i searched for methods to find whether an element is a member of
    the cell 'ismember' it gives the binary response which can then be
    converted to index using the 'find' function and then we can remove the
    members of the cell according to the condition

--- code/duplicates.m
+++ code/duplicates.m
@@ -8,5 +8,13 @@
 %  Output dups is Empty cell array: 0-by-1
 
 function dups = duplicates(strs)
-  dups = strs;
+  unq=unique(strs);
+  for m=1:length(unq)
+      k=find(ismember(strs,unq(m)));
+      if(length(k)>1)
+          k(1)=[];
+      end
+      strs(k)=[];
+  end
+  dups=strs;
 end
