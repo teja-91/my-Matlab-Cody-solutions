9fbfc94cb3e3c6c91eda0b59232e0b3ff2edecce
teja-91 <raviteja.chatta@gmail.com>
2015-08-14 13:10:11 +0530

    correctsd

---
+++ code/duplicates.m
@@ -0,0 +1,12 @@
+% Write a function that accepts a cell array of strings and returns another cell array of strings with only the duplicates retained.
+% 
+% Examples:
+% 
+%  Input  strs = {'a','b','a'}
+%  Output dups is 'a'
+%  Input  strs = {'a','b','c'}
+%  Output dups is Empty cell array: 0-by-1
+
+function dups = duplicates(strs)
+  dups = strs;
+end
