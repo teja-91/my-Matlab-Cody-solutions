e261376efd21fbd78daac6c46f2b55bc3c50c263
teja-91 <raviteja.chatta@gmail.com>
2015-10-03 19:13:47 +0530

    solved the base problem

---
+++ community/logb.m
@@ -0,0 +1,15 @@
+% Find the base b logarithm of the input decimal number x. Express the output as a decimal number. The first argument is the number x, and the second argument is the base b.
+% 
+% Examples
+% 
+%  logb(1024,2)    ---> 10
+%  logb(25,5)      --->  2
+%  logb(100000,10) --->  5
+
+function y = logb(x,b)
+y=0;
+while(x>=b)
+x=x/b;
+y=y+1
+end
+end
