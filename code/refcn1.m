5f44be4d6e71dffb46a5df6afaaf23b32da5b597
teja-91 <raviteja.chatta@gmail.com>
2015-10-16 19:51:30 +0530

    solved cody problem of removing strings that end with A

---
+++ code/refcn1.m
@@ -0,0 +1,31 @@
+% Given a list of US states, remove all the states that end with the letter A.
+% 
+% Example:
+% 
+%  Input  s1 = 'Alabama Montana Nebraska Vermont Nevada'; 
+%  Output s2 is '   Vermont '; 
+
+
+function s2 = refcn1(s1)
+s2=strsplit(s1,' ');
+del_ele=zeros(size(s2))
+repl_empt=zeros(size(s2))
+for i=1:length(s2)
+    p=cell2mat(s2(i));
+    if(p(end)=='a')
+        repl_empt(i)=1;
+        if(i>=2)
+            q=lower(cell2mat(s2(i-1)));
+            if(isequal(q,'new')||isequal(q,'north')||isequal(q,'west')||isequal(q,'south'))
+                del_ele(i-1)=1;
+            end 
+        end
+    end
+end
+repl_empt=find(repl_empt);
+for i=1:length(repl_empt)
+    s2(repl_empt)={''};
+end
+s2(find(del_ele))=[]
+s2=strjoin(s2);
+end
